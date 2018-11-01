ba9cad3ab55c418bdb0a4ca2c232fda196c9b8f1
Duman <telmand28@gmail.com>
2018-11-01 20:18:21 +0300

    create

--- week 6/script1.js
+++ week 6/script1.js
@@ -1,9 +1,10 @@
-let arr = new Array();
-let tasks = document.querySelector("#tasks")
-let divs = tasks.querySelectorAll("div");
-for (let i = 0; i < divs.length; i++) {
-  let but = divs[i].querySelector("button");
-  but.addEventListener("click", function(e){
-    divs[i].dataset.status = 'done';
-  });
+function main(event){
+	let btn = event.currentTarget;
+	let div = btn.parentNode;
+	let array = div.childNodes;
+	array[1].style.textDecoration = "line-through";
+}
+let buttons = document.querySelectorAll('button');
+for(let button of buttons){
+	button.addEventListener('click',main);
 }
--- week 6/script2.js
+++ week 6/script2.js
@@ -1,5 +1,10 @@
 let countries = ["Kazakhstan","Russia","England","France"];
-let cities_by_country = {"Kazakhstan":["Almaty","Astana","Karagandy"],"Russia":["Moscow","Saint-Petersburg","Kazan"],"England":["London","Manchester","Liverpool"],"France":["Paris","Lyon","Marseille"]};
+
+let cities_by_country = {"Kazakhstan":["Almaty","Astana","Karagandy"],
+                         "Russia":["Moscow","Saint-Petersburg","Kazan"],
+                         "England":["London","Manchester","Liverpool"],
+                         "France":["Paris","Lyon","Marseille"]};
+
 let countriesDoc = document.querySelector("#countries");
 let citiesDoc = document.querySelector("#cities");
 
@@ -11,7 +16,7 @@ for (let i = 0; i < countries.length; i++) {
 
 let whichCountry;
 countriesDoc.addEventListener("change", function(e) {
-    whichCountry = e.currentTarget.value;
+    whichCountry = e.currentTarget.value;   
     let options = citiesDoc.querySelectorAll("option");
     for (let i = 0; i < options.length; i++) {
         options[i].remove();
